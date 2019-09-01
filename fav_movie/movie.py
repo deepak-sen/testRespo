452020079d4b3704d815ed9fd00bdb5e91022502
deepak-sen <deepaksen454@gmail.com>
2019-09-01 08:00:46 +0545

    Adding files

---
+++ fav_movie/movie.py
@@ -0,0 +1,13 @@
+class Person:
+	def __init__(self, name, age):
+		self.person_name = name
+		self.person_age = age
+
+	def birthday(self):
+		self.person_age += 1
+
+	def getName(self):
+		return self.person_name
+
+bob = Person('Bob', 32)
+print(bob.getName())
